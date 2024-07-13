d3e6c476cea3374e5abbfa3aee6c6d964c6a0dbe
futuredeveloperofweb <othmane.alaouimekkaoui@gmain.com>
2024-07-13 20:09:49 +0100

    give the project a home page

--- quiz_application/urls.py
+++ quiz_application/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("quiz/", include("apps.quiz.urls", namespace="quiz")),
     path("authentication/", include("apps.authentication.urls", namespace="authentication")),
+    path('', views.home, name='home'),
 ]
